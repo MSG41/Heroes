285abb0506c460123810faa62cd5d0e43c9084c6
MSG41 <s.thaifa@amsterdam.nl>
2022-10-07 15:03:10 +0200

    start tia branche and setup client.js

--- datalayer/contentful/client.js
+++ datalayer/contentful/client.js
@@ -6,7 +6,13 @@ function isServer() {
 
 const serverOnlyCreateClient = () => {
   if (!isServer()) return;
+  
+  
   const client = createClient({
+    // space: process.env.CONTENTFUL_SPACE_ID,
+    // accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN,
+    // environment: process.env.CONTENTFUL_ENVIRONMENT,
+
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN,
     environment: process.env.CONTENTFUL_ENVIRONMENT,
