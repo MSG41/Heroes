16fabcccb8a42db4794fe9691fbfdb2b97670f81
MSG41 <s.thaifa@amsterdam.nl>
2022-10-22 12:51:54 +0200

    rich text hero details

--- components/data/details/HeroDetails.jsx
+++ components/data/details/HeroDetails.jsx
@@ -1,70 +1,68 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-import { ArrowRightIcon, ChevronLeftIcon } from '@heroicons/react/solid';
+import { ArrowRightIcon, ChevronLeftIcon } from "@heroicons/react/solid";
 
-import HeroCard from '../cards/HeroCard';
+import HeroCard from "../cards/HeroCard";
 
 const HeroDetails = ({ hero }) => {
   return (
-    <div className='px-4 sm:px-6 lg:px-8 py-8 w-full'>
+    <div className="px-4 sm:px-6 lg:px-8 py-8 w-full">
       {/* Page content */}
-      <div className='max-w-7xl mx-auto flex flex-col lg:flex-row lg:space-x-8 xl:space-x-16'>
+      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row lg:space-x-8 xl:space-x-16">
         {/* Content */}
         <div>
-          <div className='mb-6'>
-            <Link href='/'>
-              <a className='inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 shadow-lg shadow-indigo-900/40'>
-                <ChevronLeftIcon className='mr-3 h-5 w-5' aria-hidden='true' />
+          <div className="mb-6">
+            <Link href="/">
+              <a className="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 shadow-lg shadow-indigo-900/40">
+                <ChevronLeftIcon className="mr-3 h-5 w-5" aria-hidden="true" />
                 Back To Heroes
               </a>
             </Link>
           </div>
-          <div className='text-xs text-slate-500 italic mb-2'>
-            Posted On: <span className='font-semibold'>{hero.datePosted}</span>
-            {'  -  '}
-            Hero Name:{' '}
-            <span className='font-semibold'>{hero.heroCategory}</span>
+          <div className="text-xs text-slate-500 italic mb-2">
+            Posted On: <span className="font-semibold">{hero.datePosted}</span>
+            {"  -  "}
+            Hero Name:{" "}
+            <span className="font-semibold">{hero.heroCategory}</span>
           </div>
-          <header className='mb-4'>
+          <header className="mb-4">
             {/* Title */}
-            <h1 className='text-2xl md:text-3xl text-slate-1200 font-bold '>
+            <h1 className="text-2xl md:text-3xl text-slate-1200 font-bold ">
               {hero.heroName}
             </h1>
 
             {/* Important Job Details */}
-            <div className='md:flex justify-between items-center space-y-4 md:space-y-0 space-x-2 '>
+            <div className="md:flex justify-between items-center space-y-4 md:space-y-0 space-x-2 ">
               {/* Left side */}
-              <div className='flex items-start space-x-3 md:space-x-4 '>
+              <div className="flex items-start space-x-3 md:space-x-4 ">
                 <div>
-                  <div className='text-sm text-slate-500 pt-3'>
-                    {hero.company.city}{' '} /   {hero.crew} {' '} / {hero.region}
-
+                  <div className="text-sm text-slate-500 pt-3">
+                    {hero.company.city} / {hero.crew} / {hero.region}
                   </div>
                   {/* Skill Tags */}
                   <div>
-                    <div className='flex flex-wrap items-center pt-3'>
+                    <div className="flex flex-wrap items-center pt-3">
                       {hero.skills &&
                         hero.skills.map((skill) => (
-                          <div className='m-1' key={skill}>
+                          <div className="m-1" key={skill}>
                             <a
-                              className='text-xs inline-flex font-medium bg-indigo-100 text-indigo-600 rounded-full text-center px-2.5 py-1'
-                              href='#'
+                              className="text-xs inline-flex font-medium bg-indigo-100 text-indigo-600 rounded-full text-center px-2.5 py-1"
+                              href="#"
                             >
                               {skill}
                             </a>
                           </div>
                         ))}
-
                     </div>
                   </div>
                 </div>
               </div>
               {/* Right side */}
-              <div className='flex flex-col space-y-1 items-end '>
-                <div className='text-sm text-slate-900'>
-                  € {hero.baseAnnualSalary} / Session
+              <div className="flex flex-col space-y-1 items-end ">
+                <div className="text-sm text-slate-900">
+                  € {hero.baseAnnualSalary} / Day
                 </div>
                 {/* <div className='flex items-center space-x-4 pl-10 md:pl-0'>
                   {hero.featuredJob && (
@@ -91,23 +89,25 @@ const HeroDetails = ({ hero }) => {
           </header>
 
           {/* Company information (mobile) */}
-           <div className='bg-white p-5 shadow-lg rounded-sm border border-slate-200 mb-0 lg:hidden'>
-            <div className='text-center mb-0'>
-              <div className='inline-flex mb-3'>
+          <div className="bg-white p-5 shadow-lg rounded-sm border border-slate-200 mb-0 lg:hidden">
+            <div className="text-center mb-0">
+              <div className=" mb-3 ">
                 <Image
-                 className="w-[600px] h-[500px] rounded-full "
-                 src={hero.foto.url}
-                 width="500px"
-                 height="500px"
-                 alt={`${hero.heroName} - ${hero.foto.alt}`}
+                  className="w-[600px] h-[500px] rounded-full "
+                  src={hero.foto.url}
+                  width="500px"
+                  height="500px"
+                  alt={`${hero.heroName} - ${hero.foto.alt}`}
                 />
               </div>
-              <div className='text-lg font-bold text-slate-800 mb-1'>
+              <div className="text-lg font-bold text-slate-800 mb-1">
                 {hero.heroName}
               </div>
               <div className="text-emerald-500/70	 text-slate-500 font-serif hover:font-serif hover:text-violet-500/90 transition duration-500 hover:ease-in hover:ease-out">
-                <a className='break-all italic' href={hero.socials}>{hero.socials}</a></div>
-            
+                <a className="break-all italic" href={hero.socials}>
+                  {hero.socials}
+                </a>
+              </div>
             </div>
             {/* <div className='space-y-2 sm:flex sm:space-y-0 sm:space-x-2'>
               <a
@@ -136,35 +136,35 @@ const HeroDetails = ({ hero }) => {
             </div> */}
           </div>
 
-          {/* <hr className='my-6 border-t border-slate-200' />  */}
+          <hr className="my-6 border-t border-slate-200" />
 
-          {/* The Role */}
-          {/* <div>
-            <h2 className='text-xl leading-snug text-slate-800 font-bold mb-2'>
-              The Role
+          {/* The Hero Bio */}
+          <div>
+            <h2 className="text-xl leading-snug text-slate-800 font-bold mb-2 ">
+              Hero BIO
             </h2>
             <div
-              className='space-y-6 text-slate-500 font-normal text-sm'
+              className=" space-y-6 text-slate-500 font-normal text-sm max-w-[500px]"
               dangerouslySetInnerHTML={{
                 __html: hero.heroDescription,
               }}
             ></div>
           </div>
-          <hr className='my-6 border-t border-slate-200' /> */}
+          <hr className="my-6 border-t border-slate-200" />
 
           {/* About You */}
           {/* <div>
-            <h2 className='text-xl leading-snug text-slate-800 font-bold mb-2'>
+            <h2 className="text-xl leading-snug text-slate-800 font-bold mb-2">
               About You
             </h2>
             <div
-              className='space-y-6 text-slate-500 font-normal text-sm'
+              className="space-y-6 text-slate-500 font-normal text-sm"
               dangerouslySetInnerHTML={{
-                __html: hero.aboutYou,
+                __html: hero.heroName,
               }}
             ></div>
           </div>
-          <hr className='my-6 border-t border-slate-200' /> */}
+          <hr className="my-6 border-t border-slate-200" /> */}
 
           {/* Your Responsabilities */}
           {/* <div>
@@ -200,7 +200,6 @@ const HeroDetails = ({ hero }) => {
               Do you have what it takes?
             </p>
             <div className='flex justify-between items-center'> */}
-
 
           {/* Apply button */}
 
@@ -267,16 +266,14 @@ const HeroDetails = ({ hero }) => {
               </div>
             </div>
           ) : null} */}
-
-
         </div>
 
         {/* Sidebar */}
-        <div className='hidden lg:block space-y-4 '>
+        <div className="hidden lg:block space-y-4 ">
           {/* Company information (desktop) */}
-          <div className='bg-white p-5 shadow-lg rounded-sm border border-slate-200 '>
-            <div className='text-center mb-0 '>
-              <div className='inline-flex mb-3  '>
+          <div className="bg-white p-5 shadow-lg rounded-sm border border-slate-200 ">
+            <div className="text-center mb-0 ">
+              <div className="inline-flex mb-3  ">
                 <Image
                   className="w-[600px] h-[500px] rounded-full "
                   src={hero.foto.url}
@@ -285,7 +282,7 @@ const HeroDetails = ({ hero }) => {
                   alt={`${hero.heroName} - ${hero.foto.alt}`}
                 />
               </div>
-              <div className='text-lg font-bold text-slate-800 mb-1'>
+              <div className="text-lg font-bold text-slate-800 mb-1">
                 {hero.heroName}
               </div>
               {/* <div className='text-sm text-slate-500 italic'>
@@ -293,7 +290,10 @@ const HeroDetails = ({ hero }) => {
               </div> */}
 
               <div className="text-emerald-500/70	 text-slate-500 font-serif hover:font-serif hover:text-violet-500/90 transition duration-500 hover:ease-in hover:ease-out">
-                <a className='break-all italic' href={hero.socials}>{hero.socials}</a></div>
+                <a className="break-all italic" href={hero.socials}>
+                  {hero.socials}
+                </a>
+              </div>
             </div>
             {/* <div className='space-y-2'>
               <a
--- datalayer/contentful/utils.js
+++ datalayer/contentful/utils.js
@@ -1,137 +1,148 @@
-import date from 'date-and-time';
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import date from "date-and-time";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 
 export const dateReducer = (dateStr) => {
-	const dateObj = date.parse(dateStr.split('T')[0], 'YYYY-MM-DD');
-	return dateObj.toDateString();
+  const dateObj = date.parse(dateStr.split("T")[0], "YYYY-MM-DD");
+  return dateObj.toDateString();
 };
 
 export const richTextReducer = (rawRichtext) => {
-	const parsedRichText = documentToHtmlString(rawRichtext);
-	let styledRichText = parsedRichText.replace('<ul>', "<ul style='list-style-type: circle;'>");
-	return styledRichText;
+  const parsedRichText = documentToHtmlString(rawRichtext);
+  let styledRichText = parsedRichText.replace(
+    "<ul>",
+    "<ul style='list-style-type: circle;'>"
+  );
+  return styledRichText;
 };
 
 export const imageReducer = (imageField) => {
-	return {
-		url: `https:${imageField.fields.file.url}`,
-		alt: imageField.fields.title,
-		height: imageField.fields.file.details.image.height,
-		width: imageField.fields.file.details.image.width,
-		contentType: imageField.fields.file.contentType
-	};
+  return {
+    url: `https:${imageField.fields.file.url}`,
+    alt: imageField.fields.title,
+    height: imageField.fields.file.details.image.height,
+    width: imageField.fields.file.details.image.width,
+    contentType: imageField.fields.file.contentType,
+  };
 };
 
 export const companyReducer = (rawCompany) => {
-	let company = { ...rawCompany.fields };
-	company.id = rawCompany.sys.id;
-	company.locale = rawCompany.sys.locale;
-	company.logo = imageReducer(rawCompany.fields.logo);
-	company.coverImage = imageReducer(rawCompany.fields.coverImage);
-	return company;
+  let company = { ...rawCompany.fields };
+  company.id = rawCompany.sys.id;
+  company.locale = rawCompany.sys.locale;
+  company.logo = imageReducer(rawCompany.fields.logo);
+  company.coverImage = imageReducer(rawCompany.fields.coverImage);
+  return company;
 };
 
 export const tagsReducer = (tagsField) => {
-	let tags = [];
+  let tags = [];
 
-	tagsField.map((rawTag) => {
-		const tag = rawTag.sys.id;
-		tags.push(tag);
-	});
+  tagsField.map((rawTag) => {
+    const tag = rawTag.sys.id;
+    tags.push(tag);
+  });
 
-	//sorting the results alphabetically ;) easy peasy.
-	tags.sort();
+  //sorting the results alphabetically ;) easy peasy.
+  tags.sort();
 
-	return tags;
+  return tags;
 };
 
 //skillsReducer
 export const skillsReducer = (parsedTags) => {
-	const skillTags = parsedTags.filter((tag) => tag.includes('skill.'));
-	const skills = skillTags.map((skillTag) => skillTag.replace('skill.', ''));
-	return skills;
+  const skillTags = parsedTags.filter((tag) => tag.includes("skill."));
+  const skills = skillTags.map((skillTag) => skillTag.replace("skill.", ""));
+  return skills;
 };
 
 // hairReducer
 export const hairReducer = (parsedTags) => {
-	const hairTags = parsedTags.filter((tag) => tag.includes('hair.'));
-	const hair = hairTags.map((hairTag) => hairTag.replace('hair.', ''));
-	return hair;
+  const hairTags = parsedTags.filter((tag) => tag.includes("hair."));
+  const hair = hairTags.map((hairTag) => hairTag.replace("hair.", ""));
+  return hair;
 };
 
 // eyeReducer
 export const eyeReducer = (parsedTags) => {
-	const eyeTags = parsedTags.filter((tag) => tag.includes('eye.'));
-	const eye = eyeTags.map((eyeTag) => eyeTag.replace('eye.', ''));
-	return eye;
+  const eyeTags = parsedTags.filter((tag) => tag.includes("eye."));
+  const eye = eyeTags.map((eyeTag) => eyeTag.replace("eye.", ""));
+  return eye;
 };
 
 // tattooReducer
 export const tattooReducer = (parsedTags) => {
-	const tattooTags = parsedTags.filter((tag) => tag.includes('tattoo.'));
-	const tattoo = tattooTags.map((tattooTag) => tattooTag.replace('tattoo.', ''));
-	return tattoo;
+  const tattooTags = parsedTags.filter((tag) => tag.includes("tattoo."));
+  const tattoo = tattooTags.map((tattooTag) =>
+    tattooTag.replace("tattoo.", "")
+  );
+  return tattoo;
 };
 
 // scarReducer
 export const scarsReducer = (parsedTags) => {
-	const scarsTags = parsedTags.filter((tag) => tag.includes('scars.'));
-	const scars = scarsTags.map((scarsTag) => scarsTag.replace('scars.', ''));
-	return scars;
+  const scarsTags = parsedTags.filter((tag) => tag.includes("scars."));
+  const scars = scarsTags.map((scarsTag) => scarsTag.replace("scars.", ""));
+  return scars;
 };
 
 // sexReducer
 export const sexReducer = (parsedTags) => {
-	const sexTags = parsedTags.filter((tag) => tag.includes('sex.'));
-	const sex = sexTags.map((sexTag) => sexTag.replace('sex.', ''));
-	return sex;
+  const sexTags = parsedTags.filter((tag) => tag.includes("sex."));
+  const sex = sexTags.map((sexTag) => sexTag.replace("sex.", ""));
+  return sex;
 };
 
 // invoiceReducer
 export const invoiceReducer = (parsedTags) => {
-	const invoiceTags = parsedTags.filter((tag) => tag.includes('invoice.'));
-	const invoice = invoiceTags.map((invoiceTag) => invoiceTag.replace('invoice.', ''));
-	return invoice;
+  const invoiceTags = parsedTags.filter((tag) => tag.includes("invoice."));
+  const invoice = invoiceTags.map((invoiceTag) =>
+    invoiceTag.replace("invoice.", "")
+  );
+  return invoice;
 };
 
 // driveReducer
 export const driveReducer = (parsedTags) => {
-	const driveTags = parsedTags.filter((tag) => tag.includes('drive.'));
-	const drive = driveTags.map((driveTag) => driveTag.replace('drive.', ''));
-	return drive;
+  const driveTags = parsedTags.filter((tag) => tag.includes("drive."));
+  const drive = driveTags.map((driveTag) => driveTag.replace("drive.", ""));
+  return drive;
 };
 
 export const heroReducer = (rawHero, parseRelatedHeroes = true) => {
-	let hero = { ...rawHero.fields };
+  let hero = { ...rawHero.fields };
 
-	hero.id = rawHero.sys.id;
-	hero.locale = rawHero.sys.locale;
-	hero.datePosted = dateReducer(rawHero.fields.datePosted);
-	hero.company = companyReducer(rawHero.fields.company);
-	hero.aboutYou = richTextReducer(rawHero.fields.aboutYou);
-	hero.remunerationPackage = richTextReducer(rawHero.fields.remunerationPackage);
-	hero.jobResponsibilities = richTextReducer(rawHero.fields.jobResponsibilities);
-	hero.jobDescription = richTextReducer(rawHero.fields.jobDescription);
-	hero.tags = tagsReducer(rawHero.metadata.tags);
-	hero.skills = skillsReducer(hero.tags);
-	hero.hair = hairReducer(hero.tags);
-	hero.eye = eyeReducer(hero.tags);
-	hero.tattoo = tattooReducer(hero.tags);
-	hero.scars = scarsReducer(hero.tags);
-	hero.sex = sexReducer(hero.tags);
-	hero.invoice = invoiceReducer(hero.tags);
-	hero.drive = driveReducer(hero.tags);
-	hero.foto = imageReducer(rawHero.fields.foto);
+  hero.id = rawHero.sys.id;
+  hero.locale = rawHero.sys.locale;
+  hero.datePosted = dateReducer(rawHero.fields.datePosted);
+  hero.company = companyReducer(rawHero.fields.company);
+  hero.aboutYou = richTextReducer(rawHero.fields.aboutYou);
+  hero.remunerationPackage = richTextReducer(
+    rawHero.fields.remunerationPackage
+  );
+  hero.jobResponsibilities = richTextReducer(
+    rawHero.fields.jobResponsibilities
+  );
+  hero.heroDescription = richTextReducer(rawHero.fields.heroDescription);
+  hero.tags = tagsReducer(rawHero.metadata.tags);
+  hero.skills = skillsReducer(hero.tags);
+  hero.hair = hairReducer(hero.tags);
+  hero.eye = eyeReducer(hero.tags);
+  hero.tattoo = tattooReducer(hero.tags);
+  hero.scars = scarsReducer(hero.tags);
+  hero.sex = sexReducer(hero.tags);
+  hero.invoice = invoiceReducer(hero.tags);
+  hero.drive = driveReducer(hero.tags);
+  hero.foto = imageReducer(rawHero.fields.foto);
 
-	const relatedHeroes = rawHero.fields.relatedHeroes || [];
+  const relatedHeroes = rawHero.fields.relatedHeroes || [];
 
-	if (!parseRelatedHeroes) {
-		hero.relatedHeroes = [];
-	} else {
-		hero.relatedHeroes = relatedHeroes.map((relatedHero) => {
-			return heroReducer(relatedHero, false);
-		});
-	}
+  if (!parseRelatedHeroes) {
+    hero.relatedHeroes = [];
+  } else {
+    hero.relatedHeroes = relatedHeroes.map((relatedHero) => {
+      return heroReducer(relatedHero, false);
+    });
+  }
 
-	return hero;
+  return hero;
 };
