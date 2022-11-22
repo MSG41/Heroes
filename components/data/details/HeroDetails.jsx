9cb0668e216353cb1270a44d8b295638d6906c35
MSG41 <s.thaifa@amsterdam.nl>
2022-11-22 01:34:19 +0100

    justify-center

--- components/data/details/HeroDetails.jsx
+++ components/data/details/HeroDetails.jsx
@@ -260,12 +260,12 @@ const HeroDetails = ({ hero }) => {
                   <div className="m-1">
                     <a
                       id="setcard"
-                      className=" link text-[250%] flex font-medium rounded-full text-center px-2.5 py-1"
+                      className=" link text-[250%] flex justify-center font-medium rounded-full text-center px-2.5 py-1"
                       download
                       href={hero.setcard?.fields?.file?.url}
                       target="_blank"
                     >
-                      <FaRegFilePdf id="pdf" />
+                      <FaRegFilePdf id="pdf" className="flex justify-center" />
                     </a>
                   </div>
                 </div>
